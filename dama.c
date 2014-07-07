1486580fbc219d0ce476919618646fb873954981
alison <alison.simiao@bol.com.br>
2014-07-06 21:54:00 -0300

    versao velha

--- dama.c
+++ dama.c
@@ -6,12 +6,12 @@
 #define p(x) printf("%s",x);
 #define n(x) printf("%d",x);
 #define s(x) scanf("%d",&x);
-int jogx1[]={0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1};
-int jogy1[]={0,1,2,3,4,5,6,7,0,1,2,3,4,5,6,7};
-int jogz1[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; 
-int jogx2[]={6,6,6,6,6,6,6,6,7,7,7,7,7,7,7,7};
-int jogy2[]={0,1,2,3,4,5,6,7,0,1,2,3,4,5,6,7}; 
-int jogz2[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; 
+int jogx1[]={0,0,0,0,1,1,1,1,2,2,2,2};
+int jogy1[]={1,3,5,7,0,2,4,6,1,3,5,7};
+int jogz1[]={0,0,0,0,0,0,0,0,0,0,0,0}; 
+int jogx2[]={5,5,5,5,6,6,6,6,7,7,7,7};
+int jogy2[]={0,2,4,6,1,3,5,7,0,2,4,6}; 
+int jogz2[]={0,0,0,0,0,0,0,0,0,0,0,0}; 
 int campo[8][8];
 void imprimir();
 void inicia(int f1,int f2);
@@ -20,30 +20,35 @@ int vere(int jogx[],int jogy[],int x,int m,int vez);
 void joga(int jogx[],int jogy[],int x,int m,int vez);
 int come(int jogx1[],int jogy1[],int jogx2[],int jogy2[],int m,int x,int vez);
 void dama();
+int vence(int *f1,int *f2);
+
 int main(){
 int vez,x,i,j,m,pass,v;
 int f1 = 0,f2 = 0;
 srand(time(NULL));
 
 for(vez=0;m = -1;vez++){
+
+v = vence(&f1,&f2);
+if(v >  0)
+break;
+
 inicia(f1,f2);
 if(vez%2==0){
 do{
 do{
-p("X : ");
+p("jogador: ");
 s(x)
 if(jogx1[x-1] == -1)
 x = 0;
-}while(x > 16 || x < 1);
+}while(x > 12 || x < 1);
 pass = 1;
 do{
 p("mover :\n");
-p("1 - cima\n");
-p("2 - baixo\n");
-p("3 - esquerda\n");
-p("4 - direita\n");
+p("1 - esquerda\n");
+p("2 - direita\n");
 s(m);
-}while(m < 1 || m > 4);
+}while(m < 1 || m > 2);
 
 joga(jogx1,jogy1,x,m,vez);
 
@@ -52,31 +57,26 @@ pass = 0;
 
 }while(pass == 0);
 dama();
-v = vence(&f1,&f2);
-if(v >  0)
-break;
-}
-else{
+}else{
 do{
-v:
+pass = 1;
 do{
-x = rand()%16;
+x = rand()%12;
+printf("jog %d ",x+13);
 if(jogx2[x] == -1)
 x = 0;
-}while(x > 16 || x < 1);
+}while(x > 12 || x < 1);
 do{
-m = rand()%4;
-}while(m < 1 || m > 4);
+m = (rand()%2)+1;
+printf("pos %d",m);
+}while(m < 1 || m > 2);
 
 joga(jogx2,jogy2,x,m,vez);
 
 if(vere(jogx2,jogy2,x,m,vez) == 0 || come(jogx2, jogy2, jogx1,jogy1,m,x,vez) == 0)
-goto v;
+pass = 0;
 }while(pass == 0);
 dama();
-v = vence(&f1,&f2);
-if(v >  0)
-break;
 }
 }
 system("clear");
@@ -86,7 +86,7 @@ return(0);
 }
 
 void inicia(int f1,int f2){
-int i,j,w=1,u=17;
+int i,j,w = 1, u = 13;
 system("clear");
 p("jogadores fora de campo :\n")
 printf(" TIME 1: %d\n",f1);
@@ -98,7 +98,7 @@ for(i=0;i<8;i++){
 	}
 }
 
-for(j=0;j<16;j++){
+for(j=0;j<12;j++){
 if(jogz1[j] == -1)
 campo[jogx1[j]][jogy1[j]] = w+100;
 else
@@ -124,7 +124,7 @@ printf("\n");
 }
 
 void normal(int jogx[],int jogy[],int x,int m,int vez){
-int asd;
+int asd,casa;
 vez %= 2;
 if(vez == 0)
 asd = 1;
@@ -132,17 +132,13 @@ else
 asd = 0;
 
 switch(m){
-case 1:
-jogx[x-asd]++;
+case 1://esquerda 1
+jogx[x-asd] -= casa;
+jogy[x-asd] += casa;
 break;
-case 2:
-jogx[x-asd]--;
-break;
-case 3:
-jogy[x-asd]++;
-break;
-case 4:
-jogy[x-asd]--;
+case 2://direita 1
+jogx[x-asd] -=casa;
+jogy[x-asd] -=casa;
 break;
 }
 }
@@ -155,15 +151,16 @@ asd = 1;
 else
 asd = 0;
 
-if(jogx[x-asd] < 0||jogx[x-asd] > 7||jogy[x-asd] < 0||jogy[x-asd] > 7){
+if(jogx[x-asd] < 0 || jogx[x-asd] > 7 || jogy[x-asd] < 0 || jogy[x-asd] > 7){
 normal(jogx,jogy,x,m,vez);
 p("fora do campo\n\n");
 pass = 0;
 }
-for(i = 0;i < 16; i++){
+for(i = 0;i < 12; i++){
 if((x-asd) != i){
 if(jogx[x-asd] == jogx[i] && jogy[x-asd] == jogy[i]){
 normal(jogx,jogy,x,m,vez);
+printf("tem alguem nesta posicao");
 pass = 0;
 }  
 }
@@ -180,28 +177,32 @@ if(jogz1[x-asd] == -1){
 p("quantas casas vai andar ")
 s(casa)
 }
+switch(m){
+case 1://esquerda 1
+jogx[x-asd] += casa;
+jogy[x-asd] -= casa;
+break;
+case 2://direita 1
+jogx[x-asd] += casa;
+jogy[x-asd] += casa;
+break;
+}
 }
 else{
 asd = 0;
 if(jogz2[x-asd] == -1)
 casa = rand()%5;
-}
-/*for(i=0;i<casa;i++)
-if(jogx[x-asd]+i
-*/
+
 switch(m){
-case 1:
-jogx[x-asd]-= casa;
+case 1://esquerda 2
+jogx[x-asd] -= casa;
+jogy[x-asd] -= casa;
 break;
-case 2:
-jogx[x-asd]+=casa;
+case 2://direita 2
+jogx[x-asd] -= casa;
+jogy[x-asd] += casa;
 break;
-case 3:
-jogy[x-asd]-=casa;
-break;
-case 4:
-jogy[x-asd]+=casa;
-break;
+}
 }
 }
 
@@ -209,19 +210,26 @@ int come(int jogx1[],int jogy1[],int jogx2[],int jogy2[],int m,int x,int vez){
 int i,j,pass = 1;
 vez = vez%2;
 
-for(i=0;i<16;i++){
-for(j=0;j<16;j++){
+for(i=0;i<12;i++){
+for(j=0;j<12;j++){
 if(jogx1[i] ==  jogx2[j] &&  jogy1[i] == jogy2[j]){
 if(vez == 0){
-if(m == 2 && campo[jogx1[i]+1][jogy1[i]] == 0){
+if(campo[jogx1[i]-1][jogy1[i]+1] == 0 || campo[jogx1[i]+1][jogy1[i]+1] == 0){
+if(m==1){
+jogx1[i]--;
+jogy1[i]++;
+}
+if(m==2){
 jogx1[i]++;
+jogy1[i]++;
+}
 jogx2[j]= -1;
 } else{
 normal(jogx1,jogy1,x,m,vez);
 pass = 0;
 }
 } else{
-if(m == 1 && campo[jogx1[i]-1][jogy1[i]] == 0){
+if(campo[jogx1[i]-1][jogy1[i]] == 0){
 jogx1[i]--;
 jogx2[j]=-1;
 } else{
@@ -237,7 +245,7 @@ return(pass);
 
 void dama(){
 int i;
-for(i=0;i<16;i++){
+for(i = 0;i < 12;i++){
 if(jogx1[i] == 7)
 jogz1[i] = -1;
 if(jogx2[i] == 0)
@@ -265,29 +273,3 @@ t = 0;
 
 return(t);
 }
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
